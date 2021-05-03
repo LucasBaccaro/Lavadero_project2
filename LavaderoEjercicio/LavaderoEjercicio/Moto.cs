5dcd5233aeb4e5098b645053e71e96df48d12da2
LucasBaccaro <68014426+LucasBaccaro@users.noreply.github.com>
2021-05-02 22:13:38 -0300

    Create Moto.cs

---
+++ LavaderoEjercicio/LavaderoEjercicio/Moto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LavaderoEjercicio
+{
+    public class Moto:Vehiculo
+    {
+        protected float cilindrada;
+
+        public Moto(float cilindrada , string patente, Byte cantRuedas, eMarca marca) : base(patente, cantRuedas, marca)
+        {
+            this.cilindrada = cilindrada;
+        }
+
+        public string MostrarMoto()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Cantidad de cilindradas: " + this.cilindrada);
+            sb.AppendLine(Mostrar());
+            return sb.ToString();
+        }
+    }
+}
