cead9a94cf27261261479a97d5ff69d77e997c1e
LucasBaccaro <68014426+LucasBaccaro@users.noreply.github.com>
2021-05-02 22:13:16 -0300

    Create Camion.cs

---
+++ LavaderoEjercicio/LavaderoEjercicio/Camion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LavaderoEjercicio
+{
+    public class Camion:Vehiculo
+    {
+        protected float tara;
+
+        public Camion(float tara, string patente, Byte cantRuedas, eMarca marca) : base(patente, cantRuedas, marca)
+        {
+            this.tara = tara;
+        }
+
+        public string MostrarCamion()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Cantidad de taras: " + this.tara);
+            sb.AppendLine(Mostrar());
+            return sb.ToString();
+        }
+    }
+}
