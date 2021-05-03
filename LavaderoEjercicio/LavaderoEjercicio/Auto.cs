c6731f319047fbc3e6487c19196e17bf925220bb
LucasBaccaro <68014426+LucasBaccaro@users.noreply.github.com>
2021-05-02 22:13:05 -0300

    Create Auto.cs

---
+++ LavaderoEjercicio/LavaderoEjercicio/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LavaderoEjercicio
+{
+    public class Auto : Vehiculo
+    {
+        protected int cantidadAsientos;
+
+        public Auto(int asientos, string patente, Byte cantRuedas, eMarca marca) : base(patente, cantRuedas, marca)
+        {
+            this.cantidadAsientos = asientos;
+        }
+
+        public string MostrarAuto()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Cantidad de asientos: " + this.cantidadAsientos);
+            sb.AppendLine(Mostrar());
+            return sb.ToString();
+        }
+
+
+    }
+}
